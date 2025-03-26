5beb668ce7ee4f1749cc10586015ce7dbbc07210
Francisco Rodrigues <franciscofrodrigues28@gmail.com>
2025-03-26 18:31:17 +0000

    Repo Structure

--- modules/segmentation/__init__.py
+++
@@ -1,2 +0,0 @@
-from .fastsam import main as seg_fastsam
-from .mask2former import main as seg_mask2former
---
+++ server/modules/classification/classification.py
@@ -0,0 +1,52 @@
+from ultralytics import YOLO
+import os
+import json
+
+# Lista de imagens de input
+def img_list(inputs_path):
+    path_list = []
+    for root, dirs, files in os.walk(inputs_path):
+        for file in files:
+            img_path = os.path.join(root, file)
+            path_list.append(img_path)
+    return path_list
+
+def get_labels(json_file):
+    result_list = json.loads(json_file)
+    return result_list[0]['name'] if result_list else ""
+
+# Classificação de VÁRIAS imagens
+def classification_batch(model, img_paths, outputs_path):
+    results = model(img_paths, device='cpu', save=True, project=outputs_path, name='results', imgsz=320, conf=0.2, iou=0.7) # Inferência
+
+    labels = []
+    for result in results:                        
+        json_file = result.to_json() # "name", "class", "confidence", "box" (x1,y1,x2,y2)        
+        print(json_file)
+        instance_label = get_labels(json_file) # Categoria
+        if instance_label:
+            labels.append(instance_label)
+
+        # result.show()
+
+    # print("Categorias:", labels)
+    return labels
+
+# ------------------------------------------------------------------------------
+
+def main(seg_model, weights_path, inputs_path, outputs_path):
+    # Inicializar o modelo
+    model = YOLO(weights_path)
+
+    img_paths = img_list(inputs_path)
+    labels = classification_batch(model, img_paths, outputs_path)
+    return labels
+
+if __name__ == "__main__":
+    main()
+
+# probs = result.probs  # Probs object for classification outputs
+# boxes = result.boxes  # Boxes object for bounding box outputs
+# masks = result.masks  # Masks object for segmentation masks outputs
+# keypoints = result.keypoints  # Keypoints object for pose outputs
+# obb = result.obb  # Oriented boxes object for OBB outputs
